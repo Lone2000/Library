7cea0185c6935955170a9877ca9202c890b0b1ab
LoneFam <Lone0059@gmail.com>
2023-07-05 15:28:56 +0500

    Delete functionality added

--- script.js
+++ script.js
@@ -34,23 +34,23 @@ function addBookToLibrary(title, author, pages, readInput){
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook)
 
-    renderBookElement()
+    renderBookElement(newBook)
 }
 
 // Dom manipulation
 let main = document.querySelector('main')
 
 
-function renderBookElement(){
+function renderBookElement(Book){
 
     
     let book_card = document.createElement("div")
     book_card.classList.add("book-card")
     book_card.innerHTML = `
             <div class="description">
-                <h2>Harry Potter</h2>
-                <h3>by JK Rolling</h3>
-                <p>Pages: 312</p>
+                <h2>${Book.title}</h2>
+                <h3>by ${Book.author}</h3>
+                <p>Pages: ${Book.pages}</p>
             </div>
             <div class="action-btns">
                 <button class="read">Not Read</button>
@@ -61,3 +61,17 @@ function renderBookElement(){
     //  Append to main
     main.appendChild(book_card);
 }
+
+
+// Delete button
+
+// Delete event listener
+main.addEventListener('click', (e)=>{
+    // See Which book card button was clicked
+    if(e.target.classList.contains('del')){
+        // Find which card
+        const book_card = e.target.parentElement.parentElement
+        // Remove this
+        book_card.remove()
+    }
+})
